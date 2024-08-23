45552804ed1469375b104b4344930e053e9b08b2
higorluna <higorluna.dev@gmail.com>
2024-08-23 15:27:24 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,18 @@
+![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
+# Sobre o projeto:
+
+### Um amigo que tem uma loja de roupas precisava cadastras uma lista de contatos em um boot para whatsapp. Porém sua lista de contatos estava cadastrada de uma forma diferente do que o programa precisava. 
+
+(81)91234-5678 estava dessa forma.
+
+5581912345678 precisava estar assim.
+
+### Para não ter que fazer essa mudança manualmente em um excel de mais de 3.000 contatos. Fiz essa automação para ele utilizando python.
+
+### Linguagem principal: 
+Python
+### Bibliotecas usadas: 
+Pandas - para manipular os arquivos excel.
+Tkinter - para fazer a interface do usuário 
+### Ferramentas: 
+PyInstaller - para gerar o executável
---
+++ ReplaceExcel.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
+
+def alterExcel(route):
+    
+    newRoute = route.replace('"','')
+    df = pd.read_excel(newRoute, header=None)
+
+    for x in range(0, len(df)):
+        phoneNumber = df.loc[x][0]
+        alterNumber = phoneNumber.replace('(','').replace(')','').replace('-','')
+        finalNumber = "55" + alterNumber
+        df.iloc[x][0] = finalNumber
+        
+    print(df)
+    df.to_excel("contatos_atualizado.xlsx", index=False)
+    
+def on_button_click():
+    road = entry.get()
+    messagebox.showinfo("Resultado", f"Lista atualizado com sucesso")
+    alterExcel(road)
+
+# Criando a janela principal
+root = tk.Tk()
+root.title("Atualizar contatos no Excel")
+
+# Criando um rótulo (label)
+label = tk.Label(root, text="Cole o caminho da pasta Excel:")
+label.pack(pady=10)
+
+# Criando uma caixa de entrada (entry)
+entry = tk.Entry(root)
+entry.pack(pady=10)
+
+# Criando um botão
+button = tk.Button(root, text="Clique aqui", command=on_button_click)
+button.pack(pady=10)
+# Iniciando o loop principal da interface
+
+root.mainloop()
+
+print(on_button_click)
